d1ef7b264d48b3ce16e5bb087a944d486a90b8de
Esteban Toro <estebantoro.greenman@gmail.com>
2022-02-26 21:23:59 +0100

    writing file with found jobs

--- compu-scraper.py
+++ compu-scraper.py
@@ -1,20 +1,54 @@
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
 
-html_text = requests.get('https://www.computrabajo.com.co/trabajo-de-nodejs').text
+#User input
+role_user = str(input("Which job you are you looking for? > "))
+
+#Getting to the site. Filter included
+html_text = requests.get('https://www.computrabajo.com.co/trabajo-de-'+role_user).text
 soup = BeautifulSoup(html_text, 'lxml')
 posts = soup.find_all('article', class_='box_border hover dFlex vm_fx mbB cp bClick mB_neg_m mb0_m')
 
-for p in posts:
-    # print(p.text)
+#Initial vars and repository
+counter = 0
+jobs= []
+today = date.today()
+today_format= today.strftime("%B %d, %Y")
 
-    job_title = p.find("h1", class_="fs18 fwB").text
-    job_company = p.find("p", class_="fs16 fc_base mt5 mb10").text
-    job_description= p.find("p", class_="fc_aux t_word_wrap mb10 hide_m").text
+#Looping through posts  
+for p in posts:
     job_pub_time= p.find("p", class_="fs13 fc_aux").text
+    job_title = p.find("h1", class_="fs18 fwB").text.replace(' ', '')
+    job_company = p.find("p", class_="fs16 fc_base mt5 mb10").text.replace(' ', '')
+    job_description= p.find("p", class_="fc_aux t_word_wrap mb10 hide_m").text.replace(' ', '')
+    counter = counter + 1 
+    line = f'Title: {job_title} \n Date published: {job_pub_time} \n Company: {job_company} '
+
+    jobs.append(line)
 
     print("WHEN WAS IT PUBLISHED?   " + job_pub_time)
-    print("WHAT IS THE ROLE??   " + job_title)
+    print("WHAT IS THE ROLE?   " + job_title)
     print("WHICH COMPANY?   " + job_company)
     print("ARE THERE DETAILS?   " + job_description)
     print("________________________________________________")
+
+
+#Generating a report in .txt file
+f = open("jobs-report.txt", "w")
+f.write(f"""
+MY COMPUTRABAJO REPORT! \n
+Date generated: {today_format} \n 
+Info: {counter} jobs found for {role_user} \n
+This report is a product of a Python programm to scrap
+Computrabajo web portal. At first it asks users to input
+the job role they want and concatenates it to the route
+filter. 
+""")
+f.write('__________________________________________________________ \n \n')
+for job in jobs:
+    f.write(job + "\n") 
+    f.write("________________________________________________ \n")
+f.close()
+
+
